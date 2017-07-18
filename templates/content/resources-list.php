597555c355def29146938f5d8808428d04b982c4
Harry Francis <harry@deckchair.co.uk>
2017-07-18 13:53:24 +0100

    added background to filter bar on resources

--- templates/content/resources-list.php
+++ templates/content/resources-list.php
@@ -1,10 +1,9 @@
 <div class="resources-page">
-	<div class="container">
+	<div class="resources-filters">
+		<div class="container">
 
-		<div class="row">
-			<div class="col">
-				<?php $resources = get_resources(6); ?>
-					
+			<div class="row">
+				<div class="col-12">
 					<!-- Filters -->
 					<form action="" class="filters filters--resource" method="get">
 						<input type="text" name="search" placeholder="Search..." value="<?= get_query_var('search'); ?>" class="search">
@@ -25,24 +24,36 @@
 							<a href="<?php the_permalink(); ?>" class="button button--primary reset hidden">Reset</a>
 						<?php endif ?>
 					</form>
-
-					<!-- Resource listing -->
-					<div class="resources" id="resource-list">
-					<?php if ( $resources->have_posts() ) : ?>
-					    <?php while ( $resources->have_posts() ) : $resources->the_post(); ?>
-							<?php get_template_part('templates/content/resources', 'list-item'); ?>
-					    <?php endwhile; ?>
-					<?php else:  ?>
-						<div class="resource no-results">
-							<p>Sorry, we couldn't find any resources. Please <a href="/contact">get in touch</a> if you feel something is missing.</p>
-						</div>
-					<?php endif; ?>
-					
-					<?php pagination($resources->max_num_pages, '8'); ?>
-					</div>
-				<?php wp_reset_postdata(); ?>
+				</div>
 			</div>
+			
 		</div>
-		
+	</div>
+
+	<div class="resources-block">
+		<div class="container">
+
+			<div class="row">
+				<div class="col-12">
+					<?php $resources = get_resources(6); ?>
+						<!-- Resource listing -->
+						<div class="resources" id="resource-list">
+						<?php if ( $resources->have_posts() ) : ?>
+						    <?php while ( $resources->have_posts() ) : $resources->the_post(); ?>
+								<?php get_template_part('templates/content/resources', 'list-item'); ?>
+						    <?php endwhile; ?>
+						<?php else:  ?>
+							<div class="resource no-results">
+								<p>Sorry, we couldn't find any resources. Please <a href="/contact">get in touch</a> if you feel something is missing.</p>
+							</div>
+						<?php endif; ?>
+						
+						<?php pagination($resources->max_num_pages, '8'); ?>
+						</div>
+					<?php wp_reset_postdata(); ?>
+				</div>
+			</div>
+			
+		</div>
 	</div>
 </div>
