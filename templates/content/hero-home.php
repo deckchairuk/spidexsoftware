e97a1cea3173287bdacbd3d18b309835cd951093
Harry Francis <harry@deckchair.co.uk>
2017-07-18 14:59:04 +0100

    changed home hero to 10 cols

--- templates/content/hero-home.php
+++ templates/content/hero-home.php
@@ -10,7 +10,7 @@
 	<div class="container">
 		<div class="row">
 			
-			<div class="col">
+			<div class="col-12 col-md-10 offset-md-1">
 				<div class="hero__inner">
 					
 					<h1><?= $heading ?></h1>
