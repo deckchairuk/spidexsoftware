037147dafc9784167cd0fcdcbcc7f283b73d3b76
Harry Francis <harry@deckchair.co.uk>
2017-10-11 12:29:02 +0100

    added 404 page

--- 404.php
+++ 404.php
@@ -1,3 +1,22 @@
 <?php get_template_part('head'); ?>
-	404
+
+<div class="content">
+	<div class="container">
+
+		<div class="row">
+			<div class="col-12 offset-lg-1 col-lg-10">
+
+				<div class="content__inner">
+
+					<h1 class="title">404 - Page not found</h1>
+					<h3><a href="/" class="">Take me home</a></h3>
+
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+</div>
+
 <?php get_template_part('footer'); ?>
